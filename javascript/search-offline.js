4b88c3a2613fc3d42afbef781281add28541f04c
Elijah <welchelijah07@gmail.com>
2025-01-08 18:22:08 -0500

    Added Tangled to Cosmos+

--- javascript/search-offline.js
+++ javascript/search-offline.js
@@ -10,6 +10,14 @@ const movies = [
 },
 
 {
+  title: "Tangled",
+  releaseDate: "2010-11-24",
+  rating: 7.6,
+  banner: "movies/tangled/content/poster-mid.webp",
+  link: "movies/tangled/splash.html"
+},
+
+{
   title: "Storks",
   releaseDate: "2016-09-22",
   rating: 6.7,
