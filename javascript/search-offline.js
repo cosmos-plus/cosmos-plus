8f82da39c8c7dd21a93ca161f425d55b9c5a4740
Elijah <welchelijah07@gmail.com>
2025-01-08 20:03:24 -0500

    Added Catch Me If You Can to Cosmos+

--- javascript/search-offline.js
+++ javascript/search-offline.js
@@ -40,15 +40,6 @@ const movies = [
   banner: "movies/rango/content/poster-mid.webp",
   link: "movies/rango/splash.html"
 },
-
-{
-  title: "Steamboat Willie",
-  releaseDate: "1928-05-15",
-  rating: 7.3,
-  banner: "movies/steamboat-willie/content/poster-mid.webp",
-  link: "movies/steamboat-willie/splash.html"
-},
-  
 ];
 
 let moviesPerPage = 36; // Number of movies to display per page
